f2f98b189e0c02465bc419316e2805625098a29b
Muthu Priya <136813534+gmuthupriya@users.noreply.github.com>
2024-12-25 11:13:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
+
 function daysOfAYear(value) {
-	if(value%4===0)
+	if(value%4===0 &&(value%100!==0)||(value%400==0))
 	{
 		return 366;
 	}
 	else{
+		
 		return 365;
 	}
 }
-let value=parseInt(promt("Enter a year"));
-alert(daysOfAYear(value));
+console.log(daysOfAYear(2000));
