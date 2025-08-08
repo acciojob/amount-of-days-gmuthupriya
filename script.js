7b4ab9a08ab7bbbe09167af9b886193b529910a0
Muthu Priya <136813534+gmuthupriya@users.noreply.github.com>
2025-08-08 20:29:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function checkisLeapyear(year) {
+	if(year<1)||year(>9999)
+	{
+		throw new Error("Year should be within 1 to 9999")
+	}
+
+const isLeap = (year%4===0)&&(year%100!=0) || (year%400===0)
+return isLeap ? 365 : 366;
+}
